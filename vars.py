5b8583987f922e73bf3068edc8aa0ac25bc729a6
ANKITGEHLOTOP <technicalankit123@gmail.com>
2025-07-15 08:31:08 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -5,15 +5,14 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29905645"))
 API_HASH = environ.get("API_HASH", "e5a701f6e0b5fb659cb57a230b9a3feb")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7311671831:AAG_kDJkKT57WNyM3klVf8L3zzzqk7nwDMI")
-
+BOT_TOKEN = environ.get("BOT_TOKEN", "7872473343:AAGNfrMewYX6uhdcavYoioxOoECL41wxWlc")
 OWNER = int(environ.get("OWNER", "8102112566"))
-CREDIT = environ.get("CREDIT", "BAAP BOL BSDK")
+CREDIT = environ.get("CREDIT", "Tero Baap")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8102112566').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1345235576').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8102112566').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))

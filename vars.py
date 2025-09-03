fe76b42906d1e62a317cf9816e85551397bb6f82
ANKITGEHLOTOP <technicalankit123@gmail.com>
2025-09-03 23:08:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,12 +13,13 @@ user_id = environ.get("user_id", "8102112566")
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8102112566').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1345235576').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8102112566').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
